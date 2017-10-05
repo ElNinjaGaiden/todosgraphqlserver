09a983b2dd7f63be0764ffe7be011845c16bf4e1
Diego García <diego.garcia.segura@gmail.com>
2017-10-05 15:46:35 -0600

    Client url fix

--- dist/src/config.js
+++ dist/src/config.js
@@ -3,5 +3,5 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var clientUri = exports.clientUri = 'https://todosgraphql.herokuapp.com/';
+var clientUri = exports.clientUri = 'https://todosgraphql.herokuapp.com';
 var databaseConnectionString = exports.databaseConnectionString = 'postgres://pwqazskfiddlty:e49523891c5d918893cfb06556602c1eb005f30b2b7f1a82a086375dbe84f720@ec2-54-243-42-218.compute-1.amazonaws.com:5432/d9np42vapf7orv';
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const clientUri = 'https://todosgraphql.herokuapp.com/';
+export const clientUri = 'https://todosgraphql.herokuapp.com';
 export const databaseConnectionString = 'postgres://pwqazskfiddlty:e49523891c5d918893cfb06556602c1eb005f30b2b7f1a82a086375dbe84f720@ec2-54-243-42-218.compute-1.amazonaws.com:5432/d9np42vapf7orv';
